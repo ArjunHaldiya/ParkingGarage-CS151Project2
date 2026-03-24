42fe40bc91435075c79c8e47ca641c2bf3a60e50
gunrajsingh15 <gunrajsingh15@gmail.com>
2026-03-24 01:39:23 -0700

    making corrections hashams  code was missing actual class

--- src/Motorcycle.java
+++ src/Motorcycle.java
@@ -1,18 +1,82 @@
-// Name Fnu Hasham
+// Name: Fnu Hasham
 
-public double calculateParkingFee(int hours) {
-    return hours * 3.0;
-}
+//it was missing the actual class fixed that 
+public class Motorcycle extends Vehicle implements Parkable
+ {
+    private int engineSize;
+    private boolean hasHelmetStorage;
 
-public boolean canFitInCompactSpot() {
-    return true;
-}
+    public Motorcycle(String vehicleId, String licensePlate, String ownerName,
+                      int engineSize, boolean hasHelmetStorage) 
+    {
+        super(vehicleId, licensePlate, ownerName);
+        this.engineSize = engineSize;
+        this.hasHelmetStorage = hasHelmetStorage;
+    }
 
-public void displayParkingMessage() {
-    System.out.println("Motorcycle is parked successfully.");
-}
+    //getters & setters 
 
-public void printReceipt(int hours) {
-    double fee = calculateParkingFee(hours);
-    System.out.println("Motorcycle parking fee: $" + fee);
+    public int getEngineSize() 
+    {
+        return engineSize;
+    }
+
+    public void setEngineSize(int engineSize) 
+    {
+        this.engineSize = engineSize;
+    }
+
+    public boolean hasHelmetStorage() 
+    {
+        return hasHelmetStorage;
+    }
+
+    public void setHelmetStorage(boolean hasHelmetStorage) 
+    {
+        this.hasHelmetStorage = hasHelmetStorage;
+    }
+
+    @Override
+    public void parkVehicle(ParkingSpot spot) 
+    {
+        if (spot != null && spot.checkAvailability()) 
+        {
+            spot.assignVehicle(this);
+            setParked(true);
+        }
+    }
+
+    @Override
+    public void leaveSpot(ParkingSpot spot) 
+    {
+        if (spot != null) 
+        {
+            spot.removeVehicle();
+            setParked(false);
+        }
+    }
+
+    @Override
+    public double calculateParkingFee(int hours) 
+    {
+        if (hours <= 0) {
+            return 0;
+        }
+        return hours * 3.0;
+    }
+
+    @Override
+    public void displayVehicleInfo() 
+    {
+        super.displayVehicleInfo();
+        System.out.println("Engine Size: " + engineSize);
+        System.out.println("Helmet Storage: " + hasHelmetStorage);
+    }
+
+    @Override
+    public String toString() {
+        return "Motorcycle: " + super.toString() +
+               ", Engine Size: " + engineSize +
+               ", Helmet Storage: " + hasHelmetStorage;
+    }
 }
