253335783fe041a4bbf0bed06e3f1cdd12f514e0
gunrajsingh15 <gunrajsingh15@gmail.com>
2026-03-26 00:15:31 -0700

    added EV & PickupTruck classes

---
+++ src/PickupTruck.java
@@ -0,0 +1,63 @@
+public class PickupTruck extends Vehicle implements Parkable 
+{
+    private double loadCapacity;
+
+    public PickupTruck(String vehicleId, String licensePlate, String ownerName,
+                       double loadCapacity) 
+    {
+        super(vehicleId, licensePlate, ownerName);
+        this.loadCapacity = loadCapacity;
+    }
+
+    public double getLoadCapacity() 
+    {
+        return loadCapacity;
+    }
+
+    public void setLoadCapacity(double loadCapacity) 
+    {
+        if (loadCapacity > 0) {
+            this.loadCapacity = loadCapacity;
+        }
+    }
+
+    @Override
+    public void parkVehicle(ParkingSpot spot) 
+    {
+        if (spot != null && spot.checkAvailability()) {
+            spot.assignVehicle(this);
+            setParked(true);
+        }
+    }
+
+    @Override
+    public void leaveSpot(ParkingSpot spot) 
+    {
+        if (spot != null) {
+            spot.removeVehicle();
+            setParked(false);
+        }
+    }
+
+    @Override
+    //trucks would cost more to park
+    public double calculateParkingFee(int hours)
+     {
+        if (hours <= 0) return 0;
+        return hours * 7.0; 
+    }
+
+    @Override
+    public void displayVehicleInfo() 
+    {
+        super.displayVehicleInfo();
+        System.out.println("Load Capacity: " + loadCapacity + " tons");
+    }
+
+    @Override
+    public String toString() 
+    {
+        return "PickupTruck:" + super.toString() +
+               ", Load Capacity: " + loadCapacity;
+    }
+}
